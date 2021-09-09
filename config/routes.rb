0fd207100832cf7953fb9d5cf90d3734b094c8e2
Pierre Grandjean <pierrebigjean@gmail.com>
2021-09-09 16:07:07 +0200

    done routes and generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  
+  root to: 'pages#home'
+
+  resources :plants, only: [ :index, :create ]
+
+  resources :locations, only: [ :index, :create, :update, :destroy ] do
+    resources :ownerships, only: [ :create, :destroy ]
+  end
+
 end
