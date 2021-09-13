175c59753fdb4c684f8bd698ed728ab2c9a604fa
Pierre Grandjean <pierrebigjean@gmail.com>
2021-09-14 00:28:36 +0200

    plant ownership in progress

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,2 +1,33 @@
 class OwnershipsController < ApplicationController
+
+  def index
+    @ownerships = ownership.all
+    @ownership = Ownership.new
+  end
+
+  def create
+    @ownership = Ownership.new(ownerships_params)
+    @ownership.location = Plant.find(params[:location_id])
+    @ownership.save
+    redirect_to ownerships_path, notice: "Your ownership has been registered"
+  end
+
+  def update
+    @ownership = Ownership.find(params[:id])
+    @ownership.update(ownerships_params)
+    redirect_to ownerships_path, notice: "Your ownership has been successfully updated"
+  end
+
+  def destroy
+    @ownership = Ownership.find(params[:id])
+    @ownership.destroy
+    redirect_to ownerships_path, notice: "Your ownership has been deleted"
+  end
+  
+  private
+
+  def ownerships_params
+    params.require(:ownership).permit(:last_watering_date, :plant)
+  end
+
 end
