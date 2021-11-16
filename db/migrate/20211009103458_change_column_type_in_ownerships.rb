9faced034bbb6bdbe5b03302db2bff7362b24890
Pierre Grandjean <pierrebigjean@gmail.com>
2021-11-16 11:12:13 +0100

    remove migrations with issues for production

--- db/migrate/20211009103458_change_column_type_in_ownerships.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTypeInOwnerships < ActiveRecord::Migration[6.1]
-  def change
-    change_column(:ownerships, :last_watering_date, :string)
-  end
-end
--- db/migrate/20211115104656_remove_watering_dates_to_ownerships.rb
+++
@@ -1,5 +0,0 @@
-class RemoveWateringDatesToOwnerships < ActiveRecord::Migration[6.1]
-  def change
-    remove_column(:ownerships, :watering_dates)
-  end
-end
